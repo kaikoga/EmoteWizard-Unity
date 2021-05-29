6ef007446bf00075cc6c95f2b3d7871a123ab6e0
kaikoga <info@mail.kaikoga.net>
2021-05-29 21:53:57 +0900

    try to update avatar descriptor

--- EasyModeAvatar/Assets/EmoteWizard/Editor/EmoteWizard/EmoteWizardRoot.Editor.cs
+++ EasyModeAvatar/Assets/EmoteWizard/Editor/EmoteWizard/EmoteWizardRoot.Editor.cs
@@ -1,5 +1,7 @@
+using EmoteWizard.Tools;
 using UnityEditor;
 using UnityEngine;
+using VRC.SDK3.Avatars.Components;
 using static EmoteWizard.Tools.EmoteWizardEditorTools;
 
 namespace EmoteWizard
@@ -33,6 +35,65 @@ namespace EmoteWizard
                     EnsureComponent<SetupWizard>();
                 }
             }
+            var avatarDescriptor = emoteWizardRoot.avatarDescriptor;
+            if (avatarDescriptor)
+            {
+                if (GUILayout.Button("Update Avatar"))
+                {
+                    UpdateAvatar(avatarDescriptor);
+                }
+            }
+        }
+
+        void UpdateAvatar(VRCAvatarDescriptor avatarDescriptor)
+        {
+            avatarDescriptor.customizeAnimationLayers = true;
+            avatarDescriptor.baseAnimationLayers = new[]
+            {
+                new VRCAvatarDescriptor.CustomAnimLayer
+                {
+                    animatorController = null,
+                    isDefault = true,
+                    isEnabled = false,
+                    mask = null,
+                    type = VRCAvatarDescriptor.AnimLayerType.Base
+                },
+                new VRCAvatarDescriptor.CustomAnimLayer
+                {
+                    animatorController = null,
+                    isDefault = true,
+                    isEnabled = false,
+                    mask = null,
+                    type = VRCAvatarDescriptor.AnimLayerType.Additive
+                },
+                new VRCAvatarDescriptor.CustomAnimLayer
+                {
+                    animatorController = emoteWizardRoot.GetComponent<GestureWizard>()?.outputAsset,
+                    isDefault = false,
+                    isEnabled = false,
+                    mask = VrcSdkAssetLocator.HandsOnly(),
+                    type = VRCAvatarDescriptor.AnimLayerType.Gesture
+                },
+                new VRCAvatarDescriptor.CustomAnimLayer
+                {
+                    animatorController = null,
+                    isDefault = true,
+                    isEnabled = false,
+                    mask = null,
+                    type = VRCAvatarDescriptor.AnimLayerType.Action
+                },
+                new VRCAvatarDescriptor.CustomAnimLayer
+                {
+                    animatorController = emoteWizardRoot.GetComponent<FxWizard>()?.outputAsset,
+                    isDefault = false,
+                    isEnabled = false,
+                    mask = null,
+                    type = VRCAvatarDescriptor.AnimLayerType.FX
+                }
+            };
+            avatarDescriptor.customExpressions = true;
+            avatarDescriptor.expressionsMenu = emoteWizardRoot.GetComponent<ExpressionWizard>()?.outputAsset;
+            avatarDescriptor.expressionParameters = emoteWizardRoot.GetComponent<ParametersWizard>()?.outputAsset;
         }
 
         void EnsureComponent<T>()
