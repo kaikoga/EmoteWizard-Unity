ff4a21fb9ba700d820cc3d2ecb63bd559160601e
kaikoga <info@mail.kaikoga.net>
2021-06-26 21:46:56 +0900

    fix Provide-Ensure-Create mess 3 of 3

---
+++ EasyModeAvatar/Assets/EmoteWizard/Editor/EmoteWizard/Extensions/EmoteWizardRootExtension.EnsureAsset.cs
@@ -0,0 +1,37 @@
+using UnityEditor;
+using UnityEngine;
+
+using static EmoteWizard.Tools.EmoteWizardEditorTools;
+
+namespace EmoteWizard.Extensions
+{
+    public static partial class EmoteWizardRootExtension
+    {
+        public static T EnsureAsset<T>(this EmoteWizardRoot root, string relativePath)
+            where T : Object, new()
+        {
+            T asset = default;
+            return EnsureAsset(root, relativePath, ref asset);
+        }
+
+        public static T EnsureAsset<T>(this EmoteWizardRoot root, string relativePath, ref T asset)
+            where T : Object, new()
+        {
+            if (asset) return asset;
+            var assetPath = root.GeneratedAssetPath(relativePath);
+            var existingAsset = AssetDatabase.LoadAssetAtPath<T>(assetPath);
+            asset = existingAsset ? existingAsset : CreateAsset<T>(assetPath);
+            return asset;
+        }
+
+        static T CreateAsset<T>(string assetPath)
+            where T : Object, new()
+        {
+            var asset = new T();
+            EnsureDirectory(assetPath);
+            AssetDatabase.CreateAsset(asset, assetPath);
+            AssetDatabase.SaveAssets();
+            return asset;
+        }
+    }
+}
--- EasyModeAvatar/Assets/EmoteWizard/Editor/EmoteWizard/Extensions/EmoteWizardRootExtension.cs
+++ EasyModeAvatar/Assets/EmoteWizard/Editor/EmoteWizard/Extensions/EmoteWizardRootExtension.cs
@@ -5,38 +5,11 @@ using static EmoteWizard.Tools.EmoteWizardEditorTools;
 
 namespace EmoteWizard.Extensions
 {
-    public static class EmoteWizardRootExtension
+    public static partial class EmoteWizardRootExtension
     {
         public static AnimationClip ProvideEmptyClip(this EmoteWizardRoot root)
         {
             return EnsureAsset(root, "@@@Generated@@@Empty.anim", ref root.emptyClip);
-        }
-
-        public static T EnsureAsset<T>(this EmoteWizardRoot root, string relativePath)
-            where T : Object, new()
-        {
-            T asset = default;
-            return EnsureAsset(root, relativePath, ref asset);
-        }
-
-        public static T EnsureAsset<T>(this EmoteWizardRoot root, string relativePath, ref T asset)
-            where T : Object, new()
-        {
-            if (asset) return asset;
-            var assetPath = root.GeneratedAssetPath(relativePath);
-            var existingAsset = AssetDatabase.LoadAssetAtPath<T>(assetPath);
-            asset = existingAsset ? existingAsset : CreateAsset<T>(assetPath);
-            return asset;
-        }
-
-        static T CreateAsset<T>(string assetPath)
-            where T : Object, new()
-        {
-            var asset = new T();
-            EnsureDirectory(assetPath);
-            AssetDatabase.CreateAsset(asset, assetPath);
-            AssetDatabase.SaveAssets();
-            return asset;
         }
     }
 }
