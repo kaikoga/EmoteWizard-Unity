d622eb42ce8dd48692d5e93d1daafefc31d78ce3
kaikoga <info@mail.kaikoga.net>
2021-07-30 03:55:27 +0900

    override Action layer (may generate in future?), apparently BREAKING

--- EasyModeAvatar/Assets/Silksprite/Editor/EmoteWizard/Utils/VRCSDKAssetLocator.cs
+++ EasyModeAvatar/Assets/Silksprite/Editor/EmoteWizard/Utils/VRCSDKAssetLocator.cs
@@ -5,67 +5,72 @@ namespace Silksprite.EmoteWizard.Utils
 {
     public static class VrcSdkAssetLocator
     {
+        static T LoadAssetByGuidOrPath<T>(string guid, string path)
+        where T : Object
+        {
+            var realPath = AssetDatabase.GUIDToAssetPath(guid);
+            if (string.IsNullOrEmpty(realPath)) realPath = path;
+            return AssetDatabase.LoadAssetAtPath<T>(realPath);
+        }
+
         public static Texture2D PersonDance()
         {
-            // const string path = "Assets/VRCSDK/Examples3/Expressions Menu/Icons/person_dance.png";
-            var path = AssetDatabase.GUIDToAssetPath("9a20b3a6641e1af4e95e058f361790cb");
-            return AssetDatabase.LoadAssetAtPath<Texture2D>(path);
+            return LoadAssetByGuidOrPath<Texture2D>("9a20b3a6641e1af4e95e058f361790cb", 
+                "Assets/VRCSDK/Examples3/Expressions Menu/Icons/person_dance.png");
         }
 
         public static Texture2D ItemFolder()
         {
-            // const string path = "Assets/VRCSDK/Examples3/Expressions Menu/Icons/item_folder.png";
-            var path = AssetDatabase.GUIDToAssetPath("a06282136d558c54aa15d533f163ff59");
-            return AssetDatabase.LoadAssetAtPath<Texture2D>(path);
+            return LoadAssetByGuidOrPath<Texture2D>("a06282136d558c54aa15d533f163ff59", 
+                "Assets/VRCSDK/Examples3/Expressions Menu/Icons/item_folder.png");
         }
 
         public static AvatarMask HandsOnly()
         {
-            // const string path = "Assets/VRCSDK/Examples3/Animation/Masks/vrc_HandsOnly.mask";
-            var path = AssetDatabase.GUIDToAssetPath("b2b8bad9583e56a46a3e21795e96ad92");
-            return AssetDatabase.LoadAssetAtPath<AvatarMask>(path);
+            return LoadAssetByGuidOrPath<AvatarMask>("b2b8bad9583e56a46a3e21795e96ad92", 
+                "Assets/VRCSDK/Examples3/Animation/Masks/vrc_HandsOnly.mask");
         }
 
         public static AvatarMask HandLeft()
         {
-            // const string path = "Assets/VRCSDK/Examples3/Animation/Masks/vrc_Hand Left.mask";
-            var path = AssetDatabase.GUIDToAssetPath("7ff0199655202a04eb175de45a6e078a");
-            return AssetDatabase.LoadAssetAtPath<AvatarMask>(path);
+            return LoadAssetByGuidOrPath<AvatarMask>("7ff0199655202a04eb175de45a6e078a", 
+                "Assets/VRCSDK/Examples3/Animation/Masks/vrc_Hand Left.mask");
         }
 
         public static AvatarMask HandRight()
         {
-            // const string path = "Assets/VRCSDK/Examples3/Animation/Masks/vrc_Hand Right.mask";
-            var path = AssetDatabase.GUIDToAssetPath("903ce375d5f609d44b9f00b425d6eda9");
-            return AssetDatabase.LoadAssetAtPath<AvatarMask>(path);
+            return LoadAssetByGuidOrPath<AvatarMask>("903ce375d5f609d44b9f00b425d6eda9", 
+                "Assets/VRCSDK/Examples3/Animation/Masks/vrc_Hand Right.mask");
         }
 
         public static RuntimeAnimatorController HandsLayerController1()
         {
-            // const string path = "Assets/VRCSDK/Examples3/Animation/Controllers/vrc_AvatarV3HandsLayer.controller";
-            var path = AssetDatabase.GUIDToAssetPath("404d228aeae421f4590305bc4cdaba16");
-            return AssetDatabase.LoadAssetAtPath<RuntimeAnimatorController>(path);
+            return LoadAssetByGuidOrPath<RuntimeAnimatorController>("404d228aeae421f4590305bc4cdaba16", 
+                "Assets/VRCSDK/Examples3/Animation/Controllers/vrc_AvatarV3HandsLayer.controller");
         }
 
         public static RuntimeAnimatorController HandsLayerController2()
         {
-            // const string path = "Assets/VRCSDK/Examples3/Animation/Controllers/vrc_AvatarV3HandsLayer2.controller";
-            var path = AssetDatabase.GUIDToAssetPath("5ecf8b95a27552840aef66909bdf588f");
-            return AssetDatabase.LoadAssetAtPath<RuntimeAnimatorController>(path);
+            return LoadAssetByGuidOrPath<RuntimeAnimatorController>("5ecf8b95a27552840aef66909bdf588f", 
+                "Assets/VRCSDK/Examples3/Animation/Controllers/vrc_AvatarV3HandsLayer2.controller");
+        }
+
+        public static RuntimeAnimatorController ActionLayerController()
+        {
+            return LoadAssetByGuidOrPath<RuntimeAnimatorController>("3e479eeb9db24704a828bffb15406520", 
+                "Assets/VRCSDK/Examples3/Animation/Controllers/vrc_AvatarV3ActionLayer.controller");
         }
 
         public static RuntimeAnimatorController SittingLayerController1()
         {
-            // const string path = "Assets/VRCSDK/Examples3/Animation/Controllers/vrc_AvatarV3SittingLayer.controller";
-            var path = AssetDatabase.GUIDToAssetPath("1268460c14f873240981bf15aa88b21a");
-            return AssetDatabase.LoadAssetAtPath<RuntimeAnimatorController>(path);
+            return LoadAssetByGuidOrPath<RuntimeAnimatorController>("1268460c14f873240981bf15aa88b21a", 
+                "Assets/VRCSDK/Examples3/Animation/Controllers/vrc_AvatarV3SittingLayer.controller");
         }
 
         public static RuntimeAnimatorController SittingLayerController2()
         {
-            // const string path = "Assets/VRCSDK/Examples3/Animation/Controllers/vrc_AvatarV3SittingLayer2.controller";
-            var path = AssetDatabase.GUIDToAssetPath("74c2e15937e5c95478edd251f20e126f");
-            return AssetDatabase.LoadAssetAtPath<RuntimeAnimatorController>(path);
+            return LoadAssetByGuidOrPath<RuntimeAnimatorController>("74c2e15937e5c95478edd251f20e126f", 
+                "Assets/VRCSDK/Examples3/Animation/Controllers/vrc_AvatarV3SittingLayer2.controller");
         }
     }
 }
