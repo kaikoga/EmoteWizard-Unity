c5a7155b6801e6decfd571729f914ba49680026f
kaikoga <info@mail.kaikoga.net>
2021-08-01 22:30:30 +0900

    prepare for adding action emotes

--- EasyModeAvatar/Assets/Silksprite/Editor/EmoteWizard/Utils/SetupWizardUtils.cs
+++ EasyModeAvatar/Assets/Silksprite/Editor/EmoteWizard/Utils/SetupWizardUtils.cs
@@ -24,7 +24,7 @@ namespace Silksprite.EmoteWizard.Utils
         {
             var icon = VrcSdkAssetLocator.PersonDance();
             var expressionItems = Enumerable.Range(1, 8)
-                .Select(i => ExpressionItem.PopulateDefault(icon, expressionWizard.defaultPrefix, i));
+                .Select(i => ExpressionItem.PopulateDefault(icon, expressionWizard.defaultPrefix, "VRCEmote", i));
             if (expressionWizard.expressionItems == null) expressionWizard.expressionItems = new List<ExpressionItem>();
             expressionWizard.expressionItems.AddRange(expressionItems);
             expressionWizard.expressionItems = expressionWizard.expressionItems
