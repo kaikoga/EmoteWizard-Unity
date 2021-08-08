c4d27ab83380da69495d7e581a95ac94b9e045bd
kaikoga <info@mail.kaikoga.net>
2021-08-08 17:30:24 +0900

    disable ExpressionItems

--- EasyModeAvatar/Assets/Silksprite/Editor/EmoteWizard/Collections/ExpressionItem.HeaderDrawer.cs
+++ EasyModeAvatar/Assets/Silksprite/Editor/EmoteWizard/Collections/ExpressionItem.HeaderDrawer.cs
@@ -12,7 +12,7 @@ namespace Silksprite.EmoteWizard.Collections
         {
             using (new EditorGUI.IndentLevelScope(-EditorGUI.indentLevel))
             {
-                GUI.Label(position.UISlice(0.0f, 0.4f, 0), "Icon");
+                GUI.Label(position.UISlice(0.05f, 0.35f, 0), "Icon");
                 GUI.Label(position.UISlice(0.4f, 0.6f, 0), "Path");
                 
                 GUI.Label(position.UISlice(0.0f, 0.4f, 1), "Parameter");
