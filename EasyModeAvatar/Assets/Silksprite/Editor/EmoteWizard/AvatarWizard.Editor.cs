395b0effbde6757c8a48d96adc9f5af33950cda3
kaikoga <info@mail.kaikoga.net>
2022-05-15 10:50:30 +0900

    expose AvatarWizard.BuildAvatar()

--- EasyModeAvatar/Assets/Silksprite/Editor/EmoteWizard/AvatarWizard.Editor.cs
+++ EasyModeAvatar/Assets/Silksprite/Editor/EmoteWizard/AvatarWizard.Editor.cs
@@ -1,4 +1,3 @@
-using System;
 using Silksprite.EmoteWizard.Extensions;
 using Silksprite.EmoteWizard.UI;
 using Silksprite.EmoteWizard.Utils;
@@ -8,8 +7,6 @@ using Silksprite.EmoteWizardSupport.UI;
 using UnityEditor;
 using UnityEditor.Animations;
 using UnityEngine;
-using VRC.SDK3.Avatars.Components;
-
 using static Silksprite.EmoteWizardSupport.Tools.EmoteWizardEditorTools;
 
 namespace Silksprite.EmoteWizard
@@ -89,8 +86,7 @@ namespace Silksprite.EmoteWizard
                         {
                             if (GUILayout.Button("Generate Everything and Update Avatar"))
                             {
-                                avatarAnimator.runtimeAnimatorController = null;
-                                UpdateAvatar(avatarDescriptor);
+                                avatarWizard.BuildAvatar(avatarDescriptor);
                             }
                         });
 
@@ -156,145 +152,6 @@ namespace Silksprite.EmoteWizard
                 });
                 EmoteWizardGUILayout.Tutorial(emoteWizardRoot, "VRCAvatarDescriptorの更新を行います。\nAnimatorコンポーネントが存在するなら、それを使ってアバターのアニメーションの編集を開始することができます。");
             }
-        }
-
-        void UpdateAvatar(VRCAvatarDescriptor avatarDescriptor)
-        {
-            var emoteWizardRoot = avatarWizard.EmoteWizardRoot;
-            var parametersWizard = emoteWizardRoot.GetWizard<ParametersWizard>();
-
-            RuntimeAnimatorController SelectGestureController()
-            {
-                switch (avatarWizard.overrideGesture)
-                {
-                    case AvatarWizard.OverrideGeneratedControllerType2.Generate:
-                        return emoteWizardRoot.GetWizard<GestureWizard>()?.BuildOutputAsset(parametersWizard);
-                    case AvatarWizard.OverrideGeneratedControllerType2.Override:
-                        return avatarWizard.overrideGestureController;
-                    case AvatarWizard.OverrideGeneratedControllerType2.Default1:
-                        return VrcSdkAssetLocator.HandsLayerController1();
-                    case AvatarWizard.OverrideGeneratedControllerType2.Default2:
-                        return VrcSdkAssetLocator.HandsLayerController2();
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-            }
-
-            RuntimeAnimatorController SelectActionController()
-            {
-                switch (avatarWizard.overrideAction)
-                {
-                    case AvatarWizard.OverrideGeneratedControllerType1.Generate:
-                        return emoteWizardRoot.GetWizard<ActionWizard>()?.BuildOutputAsset();
-                    case AvatarWizard.OverrideGeneratedControllerType1.Override:
-                        return avatarWizard.overrideActionController;
-                    case AvatarWizard.OverrideGeneratedControllerType1.Default:
-                        return VrcSdkAssetLocator.ActionLayerController();
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-            }
-
-            RuntimeAnimatorController SelectFxController()
-            {
-                return emoteWizardRoot.GetWizard<FxWizard>()?.BuildOutputAsset(parametersWizard);
-            }
-
-            RuntimeAnimatorController SelectSittingController()
-            {
-                switch (avatarWizard.overrideSitting)
-                {
-                    case AvatarWizard.OverrideControllerType2.Override:
-                        return avatarWizard.overrideSittingController;
-                    case AvatarWizard.OverrideControllerType2.Default1:
-                        return VrcSdkAssetLocator.SittingLayerController1();
-                    case AvatarWizard.OverrideControllerType2.Default2:
-                        return VrcSdkAssetLocator.SittingLayerController2();
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-            }
-
-            var gestureController = SelectGestureController();
-            var actionController = SelectActionController();
-            var fxController = SelectFxController();
-            var sittingController = SelectSittingController();
-
-            avatarDescriptor.customizeAnimationLayers = true;
-            avatarDescriptor.baseAnimationLayers = new[]
-            {
-                new VRCAvatarDescriptor.CustomAnimLayer
-                {
-                    animatorController = null,
-                    isDefault = true,
-                    isEnabled = false,
-                    mask = null,
-                    type = VRCAvatarDescriptor.AnimLayerType.Base
-                },
-                new VRCAvatarDescriptor.CustomAnimLayer
-                {
-                    animatorController = null,
-                    isDefault = true,
-                    isEnabled = false,
-                    mask = null,
-                    type = VRCAvatarDescriptor.AnimLayerType.Additive
-                },
-                new VRCAvatarDescriptor.CustomAnimLayer
-                {
-                    animatorController = gestureController,
-                    isDefault = gestureController == null,
-                    isEnabled = false,
-                    mask = VrcSdkAssetLocator.HandsOnly(),
-                    type = VRCAvatarDescriptor.AnimLayerType.Gesture
-                },
-                new VRCAvatarDescriptor.CustomAnimLayer
-                {
-                    animatorController = actionController,
-                    isDefault = actionController == null,
-                    isEnabled = false,
-                    mask = null,
-                    type = VRCAvatarDescriptor.AnimLayerType.Action
-                },
-                new VRCAvatarDescriptor.CustomAnimLayer
-                {
-                    animatorController = fxController,
-                    isDefault = fxController == null,
-                    isEnabled = false,
-                    mask = null,
-                    type = VRCAvatarDescriptor.AnimLayerType.FX
-                }
-            };
-            
-            avatarDescriptor.specialAnimationLayers = new[]
-            {
-                new VRCAvatarDescriptor.CustomAnimLayer
-                {
-                    animatorController = sittingController,
-                    isDefault = false,
-                    isEnabled = false,
-                    mask = null,
-                    type = VRCAvatarDescriptor.AnimLayerType.Sitting
-                },
-                new VRCAvatarDescriptor.CustomAnimLayer
-                {
-                    animatorController = null,
-                    isDefault = true,
-                    isEnabled = false,
-                    mask = null,
-                    type = VRCAvatarDescriptor.AnimLayerType.TPose
-                },
-                new VRCAvatarDescriptor.CustomAnimLayer
-                {
-                    animatorController = null,
-                    isDefault = false,
-                    isEnabled = false,
-                    mask = null,
-                    type = VRCAvatarDescriptor.AnimLayerType.IKPose
-                }
-            };
-            avatarDescriptor.customExpressions = true;
-            avatarDescriptor.expressionsMenu = emoteWizardRoot.GetWizard<ExpressionWizard>()?.BuildOutputAsset();
-            avatarDescriptor.expressionParameters = emoteWizardRoot.GetWizard<ParametersWizard>()?.BuildOutputAsset();
         }
     }
 }
--- EasyModeAvatar/Assets/Silksprite/Editor/EmoteWizard/Extensions/AvatarWizardExtension.cs
+++ EasyModeAvatar/Assets/Silksprite/Editor/EmoteWizard/Extensions/AvatarWizardExtension.cs
@@ -1,6 +1,8 @@
+using System;
+using Silksprite.EmoteWizard.Utils;
 using Silksprite.EmoteWizardSupport.Extensions;
-using UnityEditor;
 using UnityEngine;
+using VRC.SDK3.Avatars.Components;
 
 namespace Silksprite.EmoteWizard.Extensions
 {
@@ -11,6 +13,148 @@ namespace Silksprite.EmoteWizard.Extensions
             var animator = avatarWizard.proxyAnimator ? avatarWizard.proxyAnimator : avatarWizard.avatarDescriptor.EnsureComponent<Animator>();
             avatarWizard.proxyAnimator = animator;
             return animator;
+        }
+
+        public static void BuildAvatar(this AvatarWizard avatarWizard, VRCAvatarDescriptor avatarDescriptor)
+        {
+            var avatarAnimator = avatarWizard.avatarDescriptor.EnsureComponent<Animator>();
+            avatarAnimator.runtimeAnimatorController = null;
+            
+            var emoteWizardRoot = avatarWizard.EmoteWizardRoot;
+            var parametersWizard = emoteWizardRoot.GetWizard<ParametersWizard>();
+
+            RuntimeAnimatorController SelectGestureController()
+            {
+                switch (avatarWizard.overrideGesture)
+                {
+                    case AvatarWizard.OverrideGeneratedControllerType2.Generate:
+                        return emoteWizardRoot.GetWizard<GestureWizard>()?.BuildOutputAsset(parametersWizard);
+                    case AvatarWizard.OverrideGeneratedControllerType2.Override:
+                        return avatarWizard.overrideGestureController;
+                    case AvatarWizard.OverrideGeneratedControllerType2.Default1:
+                        return VrcSdkAssetLocator.HandsLayerController1();
+                    case AvatarWizard.OverrideGeneratedControllerType2.Default2:
+                        return VrcSdkAssetLocator.HandsLayerController2();
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+
+            RuntimeAnimatorController SelectActionController()
+            {
+                switch (avatarWizard.overrideAction)
+                {
+                    case AvatarWizard.OverrideGeneratedControllerType1.Generate:
+                        return emoteWizardRoot.GetWizard<ActionWizard>()?.BuildOutputAsset();
+                    case AvatarWizard.OverrideGeneratedControllerType1.Override:
+                        return avatarWizard.overrideActionController;
+                    case AvatarWizard.OverrideGeneratedControllerType1.Default:
+                        return VrcSdkAssetLocator.ActionLayerController();
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+
+            RuntimeAnimatorController SelectFxController()
+            {
+                return emoteWizardRoot.GetWizard<FxWizard>()?.BuildOutputAsset(parametersWizard);
+            }
+
+            RuntimeAnimatorController SelectSittingController()
+            {
+                switch (avatarWizard.overrideSitting)
+                {
+                    case AvatarWizard.OverrideControllerType2.Override:
+                        return avatarWizard.overrideSittingController;
+                    case AvatarWizard.OverrideControllerType2.Default1:
+                        return VrcSdkAssetLocator.SittingLayerController1();
+                    case AvatarWizard.OverrideControllerType2.Default2:
+                        return VrcSdkAssetLocator.SittingLayerController2();
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+
+            var gestureController = SelectGestureController();
+            var actionController = SelectActionController();
+            var fxController = SelectFxController();
+            var sittingController = SelectSittingController();
+
+            avatarDescriptor.customizeAnimationLayers = true;
+            avatarDescriptor.baseAnimationLayers = new[]
+            {
+                new VRCAvatarDescriptor.CustomAnimLayer
+                {
+                    animatorController = null,
+                    isDefault = true,
+                    isEnabled = false,
+                    mask = null,
+                    type = VRCAvatarDescriptor.AnimLayerType.Base
+                },
+                new VRCAvatarDescriptor.CustomAnimLayer
+                {
+                    animatorController = null,
+                    isDefault = true,
+                    isEnabled = false,
+                    mask = null,
+                    type = VRCAvatarDescriptor.AnimLayerType.Additive
+                },
+                new VRCAvatarDescriptor.CustomAnimLayer
+                {
+                    animatorController = gestureController,
+                    isDefault = gestureController == null,
+                    isEnabled = false,
+                    mask = VrcSdkAssetLocator.HandsOnly(),
+                    type = VRCAvatarDescriptor.AnimLayerType.Gesture
+                },
+                new VRCAvatarDescriptor.CustomAnimLayer
+                {
+                    animatorController = actionController,
+                    isDefault = actionController == null,
+                    isEnabled = false,
+                    mask = null,
+                    type = VRCAvatarDescriptor.AnimLayerType.Action
+                },
+                new VRCAvatarDescriptor.CustomAnimLayer
+                {
+                    animatorController = fxController,
+                    isDefault = fxController == null,
+                    isEnabled = false,
+                    mask = null,
+                    type = VRCAvatarDescriptor.AnimLayerType.FX
+                }
+            };
+            
+            avatarDescriptor.specialAnimationLayers = new[]
+            {
+                new VRCAvatarDescriptor.CustomAnimLayer
+                {
+                    animatorController = sittingController,
+                    isDefault = false,
+                    isEnabled = false,
+                    mask = null,
+                    type = VRCAvatarDescriptor.AnimLayerType.Sitting
+                },
+                new VRCAvatarDescriptor.CustomAnimLayer
+                {
+                    animatorController = null,
+                    isDefault = true,
+                    isEnabled = false,
+                    mask = null,
+                    type = VRCAvatarDescriptor.AnimLayerType.TPose
+                },
+                new VRCAvatarDescriptor.CustomAnimLayer
+                {
+                    animatorController = null,
+                    isDefault = false,
+                    isEnabled = false,
+                    mask = null,
+                    type = VRCAvatarDescriptor.AnimLayerType.IKPose
+                }
+            };
+            avatarDescriptor.customExpressions = true;
+            avatarDescriptor.expressionsMenu = emoteWizardRoot.GetWizard<ExpressionWizard>()?.BuildOutputAsset();
+            avatarDescriptor.expressionParameters = emoteWizardRoot.GetWizard<ParametersWizard>()?.BuildOutputAsset();
         }
     }
 }
