59c6029a6e469c957114dd91d7e9d45c9c7ae827
kaikoga <info@mail.kaikoga.net>
2022-05-25 21:02:56 +0900

    disallow multiple hand sign sources

--- EasyModeAvatar/Assets/Silksprite/EmoteWizard/Sources/FxEmoteSource.cs
+++ EasyModeAvatar/Assets/Silksprite/EmoteWizard/Sources/FxEmoteSource.cs
@@ -1,7 +1,9 @@
 using Silksprite.EmoteWizard.Sources.Base;
+using UnityEngine;
 
 namespace Silksprite.EmoteWizard.Sources
 {
+    [DisallowMultipleComponent]
     public class FxEmoteSource : EmoteSourceBase
     {
         public override string LayerName => "FX";
--- EasyModeAvatar/Assets/Silksprite/EmoteWizard/Sources/GestureEmoteSource.cs
+++ EasyModeAvatar/Assets/Silksprite/EmoteWizard/Sources/GestureEmoteSource.cs
@@ -1,7 +1,9 @@
 using Silksprite.EmoteWizard.Sources.Base;
+using UnityEngine;
 
 namespace Silksprite.EmoteWizard.Sources
 {
+    [DisallowMultipleComponent]
     public class GestureEmoteSource : EmoteSourceBase
     {
         public override string LayerName => "Gesture";
