f057fd60aa30c1423d62b601d5ff52009e21a7e2
kaikoga <info@mail.kaikoga.net>
2022-10-02 23:51:38 +0900

    explode into hierarchy

--- EasyModeAvatar/Packages/net.kaikoga.emotewizard/Editor/Silksprite/EmoteWizardSupport/Extensions/UnityExtension.cs
+++ EasyModeAvatar/Packages/net.kaikoga.emotewizard/Editor/Silksprite/EmoteWizardSupport/Extensions/UnityExtension.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -10,25 +11,39 @@ namespace Silksprite.EmoteWizardSupport.Extensions
             return !string.IsNullOrEmpty(AssetDatabase.GetAssetPath(unityObject));
         }
 
-        public static T AddChildComponent<T>(this Component component, string name = null)
+        public static T AddChildComponent<T>(this Component component, string path = null)
             where T : Component
         {
-            var childObject = new GameObject(name ?? typeof(T).Name);
-            childObject.transform.parent = component.transform;
-            return childObject.AddComponent<T>();
+            var paths = string.IsNullOrEmpty(path) ? new[] { typeof(T).Name } : path.Split('/').ToArray();
+            var gameObject = component.gameObject;
+            foreach (var name in paths)
+            {
+                var childTransform = gameObject.transform.Find(name);
+                if (childTransform)
+                {
+                    gameObject = childTransform.gameObject;
+                }
+                else
+                {
+                    var newChildObject = new GameObject(name);
+                    newChildObject.transform.parent = gameObject.transform;
+                    gameObject = newChildObject;
+                }
+            }
+            return gameObject.AddComponent<T>();
         }
 
-        public static T FindOrCreateChildComponent<T>(this Component component, string name = null)
+        public static T FindOrCreateChildComponent<T>(this Component component, string path = null)
             where T : Component
         {
-            var child = component.transform.Find(name);
-            return child ? child.EnsureComponent<T>() : component.AddChildComponent<T>(name);
+            var child = component.transform.Find(path);
+            return child ? child.EnsureComponent<T>() : component.AddChildComponent<T>(path);
         }
 
-        public static T AddChildComponentAndSelect<T>(this Component component, string name = null)
+        public static T AddChildComponentAndSelect<T>(this Component component, string path = null)
         where T : Component
         {
-            var result = component.AddChildComponent<T>(name);
+            var result = component.AddChildComponent<T>(path);
             Selection.SetActiveObjectWithContext(result.gameObject, result);
             return result;
         }
