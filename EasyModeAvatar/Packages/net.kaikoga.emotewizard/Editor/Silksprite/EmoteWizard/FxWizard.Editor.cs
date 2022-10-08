7877c169cfb4a559d50959fc40f9e785818e95c9
kaikoga <info@mail.kaikoga.net>
2022-10-08 21:39:49 +0900

    remove legacy wizard editors

--- EasyModeAvatar/Packages/net.kaikoga.emotewizard/Editor/Silksprite/EmoteWizard/FxWizard.Editor.cs
+++ EasyModeAvatar/Packages/net.kaikoga.emotewizard/Editor/Silksprite/EmoteWizard/FxWizard.Editor.cs
@@ -1,7 +1,3 @@
-using Silksprite.EmoteWizard.Extensions;
-using Silksprite.EmoteWizard.UI;
-using Silksprite.EmoteWizardSupport.Scopes;
-using Silksprite.EmoteWizardSupport.UI;
 using UnityEditor;
 
 namespace Silksprite.EmoteWizard
@@ -9,55 +5,10 @@ namespace Silksprite.EmoteWizard
     [CustomEditor(typeof(FxWizard))]
     public class FxWizardEditor : Editor
     {
-        FxWizard fxWizard;
-
-        void OnEnable()
-        {
-            fxWizard = (FxWizard) target;
-        }
-
         public override void OnInspectorGUI()
         {
-            var emoteWizardRoot = fxWizard.EmoteWizardRoot;
-
-            using (new ObjectChangeScope(fxWizard))
-            {
-                var parametersWizard = emoteWizardRoot.GetWizard<ParametersWizard>();
-
-                TypedGUILayout.Toggle("HandSign Override", ref fxWizard.handSignOverrideEnabled);
-                if (fxWizard.handSignOverrideEnabled)
-                {
-                    using (new InvalidValueScope(parametersWizard.IsInvalidParameter(fxWizard.handSignOverrideParameter)))
-                    {
-                        TypedGUILayout.TextField("HandSign Override Parameter", ref fxWizard.handSignOverrideParameter);
-                    }
-                }
-                else
-                {
-                    using (new EditorGUI.DisabledScope(true))
-                    {
-                        TypedGUILayout.TextField("HandSign Override Parameter", ref fxWizard.handSignOverrideParameter);
-                    }
-                }
-
-                EmoteWizardGUILayout.OutputUIArea(() =>
-                {
-                    TypedGUILayout.AssetField("Output Asset", ref fxWizard.outputAsset);
-                    TypedGUILayout.AssetField("Reset Clip", ref fxWizard.resetClip);
-                });
-            }
-
-            EmoteWizardGUILayout.Tutorial(emoteWizardRoot, Tutorial);
-            EmoteWizardGUILayout.Tutorial(emoteWizardRoot, Tutorial2);
+            base.OnInspectorGUI();
+            EditorGUILayout.HelpBox("古いコンポーネントです。", MessageType.Warning);
         }
-
-        static string Tutorial => 
-            string.Join("\n",
-                "FX Layerの設定を行い、Animation Controllerを生成します。");
-
-        static string Tutorial2 => 
-            string.Join("\n",
-                "Write Defaultsオフでセットアップされます。",
-                "各アニメーションで使われているパラメータをリセットするアニメーションがFX Layerの一番上に追加されます。");
     }
 }
--- EasyModeAvatar/Packages/net.kaikoga.emotewizard/Editor/Silksprite/EmoteWizard/GestureWizard.Editor.cs
+++ EasyModeAvatar/Packages/net.kaikoga.emotewizard/Editor/Silksprite/EmoteWizard/GestureWizard.Editor.cs
@@ -1,70 +1,14 @@
-using Silksprite.EmoteWizard.Extensions;
-using Silksprite.EmoteWizard.UI;
-using Silksprite.EmoteWizard.Utils;
-using Silksprite.EmoteWizardSupport.Scopes;
-using Silksprite.EmoteWizardSupport.UI;
 using UnityEditor;
-using UnityEngine;
 
 namespace Silksprite.EmoteWizard
 {
     [CustomEditor(typeof(GestureWizard))]
     public class GestureWizardEditor : Editor
     {
-        GestureWizard gestureWizard;
-
-        void OnEnable()
-        {
-            gestureWizard = (GestureWizard) target;
-        }
-
         public override void OnInspectorGUI()
         {
-            var emoteWizardRoot = gestureWizard.EmoteWizardRoot;
-
-            using (new ObjectChangeScope(gestureWizard))
-            {
-                var parametersWizard = emoteWizardRoot.GetWizard<ParametersWizard>();
-
-                TypedGUILayout.Toggle("HandSign Override", ref gestureWizard.handSignOverrideEnabled);
-                if (gestureWizard.handSignOverrideEnabled)
-                {
-                    using (new InvalidValueScope(parametersWizard.IsInvalidParameter(gestureWizard.handSignOverrideParameter)))
-                    {
-                        TypedGUILayout.TextField("HandSign Override Parameter", ref gestureWizard.handSignOverrideParameter);
-                    }
-                }
-                else
-                {
-                    using (new EditorGUI.DisabledScope(true))
-                    {
-                        TypedGUILayout.TextField("HandSign Override Parameter", ref gestureWizard.handSignOverrideParameter);
-                    }
-                }
-
-                CustomTypedGUILayout.AssetFieldWithGenerate("Default Avatar Mask", ref gestureWizard.defaultAvatarMask, () =>
-                {
-                    var avatarMask = emoteWizardRoot.EnsureAsset<AvatarMask>("Gesture/@@@Generated@@@GestureDefaultMask.mask");
-                    return AvatarMaskUtils.SetupAsGestureDefault(avatarMask);
-                });
-
-                EmoteWizardGUILayout.OutputUIArea(() =>
-                {
-                    TypedGUILayout.AssetField("Output Asset", ref gestureWizard.outputAsset);
-                });
-            }
-
-            EmoteWizardGUILayout.Tutorial(emoteWizardRoot, Tutorial);
-            EmoteWizardGUILayout.Tutorial(emoteWizardRoot, Tutorial2);
+            base.OnInspectorGUI();
+            EditorGUILayout.HelpBox("古いコンポーネントです。", MessageType.Warning);
         }
-
-        static string Tutorial => 
-            string.Join("\n",
-                "Gesture Layerの設定を行い、Animation Controllerを生成します。");
-
-        static string Tutorial2 => 
-            string.Join("\n",
-                "Write Defaultsオフでセットアップされます。",
-                "各アニメーションで使われているパラメータをリセットするアニメーションがGesture Layerの一番上に追加されます。");
     }
 }
