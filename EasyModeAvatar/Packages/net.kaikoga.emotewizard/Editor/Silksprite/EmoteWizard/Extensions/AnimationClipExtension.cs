7358ccca8864c82cec8d45aa43b8bfaa8b61afa9
kaikoga <info@mail.kaikoga.net>
2022-12-04 08:24:10 +0900

    prevent updating VRC sample animations

--- EasyModeAvatar/Packages/net.kaikoga.emotewizard/Editor/Silksprite/EmoteWizard/Extensions/AnimationClipExtension.cs
+++ EasyModeAvatar/Packages/net.kaikoga.emotewizard/Editor/Silksprite/EmoteWizard/Extensions/AnimationClipExtension.cs
@@ -7,11 +7,14 @@ namespace Silksprite.EmoteWizard.Extensions
     {
         public static void SetLoopTime(this AnimationClip clip, bool value)
         {
+            var path = AssetDatabase.GetAssetPath(clip);
+            if (path.StartsWith("Packages/com.vrchat")) return;
             var serializedObject = new SerializedObject(clip);
             var property = serializedObject.FindProperty("m_AnimationClipSettings.m_LoopTime");
             if (property.boolValue == value) return;
             property.boolValue = value;
             serializedObject.ApplyModifiedProperties();
+            EditorUtility.SetDirty(clip);
         }
     }
 }
