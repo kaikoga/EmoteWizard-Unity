b464bfb2146df5765cfede32462da1ce34f57f0a
kaikoga <info@mail.kaikoga.net>
2023-11-05 03:22:44 +0900

    start defining ndmf plugin

---
+++ EasyModeAvatar/Packages/net.kaikoga.emotewizard/Editor/Silksprite/EmoteWizard/Ndmf/EmoteWizardPlugin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using nadena.dev.ndmf;
+using Silksprite.EmoteWizard.Ndmf;
+using UnityEngine;
+
+[assembly: ExportsPlugin(typeof(EmoteWizardPlugin))]
+
+namespace Silksprite.EmoteWizard.Ndmf
+{
+    class EmoteWizardPlugin : Plugin<EmoteWizardPlugin>
+    {
+        public override string QualifiedName => "net.kaikoga.emotewizard";
+        public override string DisplayName => "Emote Wizard";
+
+        protected override void OnUnhandledException(Exception e)
+        {
+            Debug.LogException(e);
+        }
+
+        protected override void Configure()
+        {
+            var seq = InPhase(BuildPhase.Generating);
+        }
+    }
+}
