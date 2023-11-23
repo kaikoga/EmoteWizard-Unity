3395b746a8ac436ae42b9fb4f6cfe804d8b5f944
kaikoga <info@mail.kaikoga.net>
2023-11-24 00:25:36 +0900

    split file

--- EasyModeAvatar/Packages/net.kaikoga.emotewizard/Runtime/Silksprite/EmoteWizard/Contexts/IBehaviourContext.cs
+++ EasyModeAvatar/Packages/net.kaikoga.emotewizard/Runtime/Silksprite/EmoteWizard/Contexts/IBehaviourContext.cs
@@ -15,35 +15,4 @@ namespace Silksprite.EmoteWizard.Contexts
     {
         T OutputAsset { get; set; }
     }
-    
-    public abstract class ContextBase<TWizard> : IBehaviourContext
-        where TWizard : EmoteWizardBase
-    {
-        protected readonly TWizard Wizard;
-
-        public EmoteWizardEnvironment Environment { get; }
-        public GameObject GameObject => Wizard.gameObject;
-
-        protected ContextBase(EmoteWizardEnvironment env)
-        {
-            Environment = env;
-        }
-
-        protected ContextBase(TWizard wizard)
-        {
-            Environment = wizard.Environment;
-            Wizard = wizard;
-        }
-
-        public abstract void DisconnectOutputAssets();
-    }
-
-    public abstract class OutputContextBase<TWizard, TOut> : ContextBase<TWizard>, IOutputContext<TOut>
-        where TWizard : EmoteWizardBase
-    {
-        protected OutputContextBase(EmoteWizardEnvironment env) : base(env) { }
-        protected OutputContextBase(TWizard wizard) : base(wizard) { }
-        public abstract TOut OutputAsset { get; set; }
-    }
-
 }
