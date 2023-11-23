5e2c67d40f74ae66271ee19a1dd0b42929e0ffeb
kaikoga <info@mail.kaikoga.net>
2023-11-23 23:11:58 +0900

    reformat code

--- EasyModeAvatar/Packages/net.kaikoga.emotewizard/Runtime/Silksprite/EmoteWizard/Contexts/ParametersContext.cs
+++ EasyModeAvatar/Packages/net.kaikoga.emotewizard/Runtime/Silksprite/EmoteWizard/Contexts/ParametersContext.cs
@@ -9,76 +9,75 @@ using VRC.SDK3.Avatars.ScriptableObjects;
 namespace Silksprite.EmoteWizard.Contexts
 {
     public class ParametersContext : IOutputContext<VRCExpressionParameters>
-
     {
-    readonly ParametersWizard _wizard;
+        readonly ParametersWizard _wizard;
 
-    public ParametersContext(ParametersWizard wizard) => _wizard = wizard;
+        public ParametersContext(ParametersWizard wizard) => _wizard = wizard;
 
-    public EmoteWizardEnvironment Environment => _wizard.Environment;
+        public EmoteWizardEnvironment Environment => _wizard.Environment;
 
-    public GameObject GameObject => _wizard.gameObject;
+        public GameObject GameObject => _wizard.gameObject;
 
-    public VRCExpressionParameters OutputAsset
-    {
-        get => _wizard.outputAsset;
-        set => _wizard.outputAsset = value;
-    }
-
-    public void DisconnectOutputAssets()
-    {
-        _wizard.outputAsset = null;
-    }
-
-    IEnumerable<EmoteItem> CollectEmoteItems()
-    {
-        return _wizard.Environment.CollectAllEmoteItems();
-    }
-
-    IEnumerable<ExpressionItem> CollectExpressionItems()
-    {
-        var expressionWizard = _wizard.Environment.GetContext<ExpressionContext>();
-        return expressionWizard != null ? expressionWizard.CollectExpressionItems() : Enumerable.Empty<ExpressionItem>();
-    }
-
-    IEnumerable<ParameterItem> CollectSourceParameterItems()
-    {
-        return _wizard.Environment.GetComponentsInChildren<IParameterSource>()
-            .SelectMany(source => source.ParameterItems);
-    }
-
-    public ParametersSnapshot Snapshot()
-    {
-        var builder = new ParameterSnapshotBuilder();
-
-        foreach (var expressionItem in CollectExpressionItems())
+        public VRCExpressionParameters OutputAsset
         {
-            if (!string.IsNullOrEmpty(expressionItem.parameter))
-            {
-                builder.FindOrCreate(expressionItem.parameter).AddWriteValue(expressionItem.value);
-            }
-
-            if (!expressionItem.IsPuppet) continue;
-            foreach (var subParameter in expressionItem.subParameters.Where(subParameter => !string.IsNullOrEmpty(subParameter)))
-            {
-                builder.FindOrCreate(subParameter).AddWritePuppet(expressionItem.itemKind == ExpressionItemKind.TwoAxisPuppet);
-            }
+            get => _wizard.outputAsset;
+            set => _wizard.outputAsset = value;
         }
 
-        foreach (var emoteItem in CollectEmoteItems())
+        public void DisconnectOutputAssets()
         {
-            foreach (var condition in emoteItem.Trigger.conditions)
+            _wizard.outputAsset = null;
+        }
+
+        IEnumerable<EmoteItem> CollectEmoteItems()
+        {
+            return _wizard.Environment.CollectAllEmoteItems();
+        }
+
+        IEnumerable<ExpressionItem> CollectExpressionItems()
+        {
+            var expressionWizard = _wizard.Environment.GetContext<ExpressionContext>();
+            return expressionWizard != null ? expressionWizard.CollectExpressionItems() : Enumerable.Empty<ExpressionItem>();
+        }
+
+        IEnumerable<ParameterItem> CollectSourceParameterItems()
+        {
+            return _wizard.Environment.GetComponentsInChildren<IParameterSource>()
+                .SelectMany(source => source.ParameterItems);
+        }
+
+        public ParametersSnapshot Snapshot()
+        {
+            var builder = new ParameterSnapshotBuilder();
+
+            foreach (var expressionItem in CollectExpressionItems())
             {
-                builder.FindOrCreate(condition.parameter).AddReadValue(condition.kind, condition.threshold);
+                if (!string.IsNullOrEmpty(expressionItem.parameter))
+                {
+                    builder.FindOrCreate(expressionItem.parameter).AddWriteValue(expressionItem.value);
+                }
+
+                if (!expressionItem.IsPuppet) continue;
+                foreach (var subParameter in expressionItem.subParameters.Where(subParameter => !string.IsNullOrEmpty(subParameter)))
+                {
+                    builder.FindOrCreate(subParameter).AddWritePuppet(expressionItem.itemKind == ExpressionItemKind.TwoAxisPuppet);
+                }
             }
-        }
 
-        foreach (var parameter in CollectSourceParameterItems())
-        {
-            builder.FindOrCreate(parameter.name).Import(parameter);
-        }
+            foreach (var emoteItem in CollectEmoteItems())
+            {
+                foreach (var condition in emoteItem.Trigger.conditions)
+                {
+                    builder.FindOrCreate(condition.parameter).AddReadValue(condition.kind, condition.threshold);
+                }
+            }
 
-        return builder.ToSnapshot();
-    }
+            foreach (var parameter in CollectSourceParameterItems())
+            {
+                builder.FindOrCreate(parameter.name).Import(parameter);
+            }
+
+            return builder.ToSnapshot();
+        }
     }
 }
