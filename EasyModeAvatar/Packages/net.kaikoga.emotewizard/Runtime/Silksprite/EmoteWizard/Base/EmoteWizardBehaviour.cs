e8db05dbd60b76eab6c95adba85e3adb20589ec6
kaikoga <info@mail.kaikoga.net>
2023-11-27 00:22:43 +0900

    set EW components Editor only

--- EasyModeAvatar/Packages/net.kaikoga.emotewizard/Runtime/Silksprite/EmoteWizard/Base/EmoteWizardBehaviour.cs
+++ EasyModeAvatar/Packages/net.kaikoga.emotewizard/Runtime/Silksprite/EmoteWizard/Base/EmoteWizardBehaviour.cs
@@ -2,10 +2,11 @@ using System.Linq;
 using Silksprite.EmoteWizard.Contexts;
 using UnityEngine;
 using VRC.SDK3.Avatars.Components;
+using VRC.SDKBase;
 
 namespace Silksprite.EmoteWizard.Base
 {
-    public abstract class EmoteWizardBehaviour : MonoBehaviour
+    public abstract class EmoteWizardBehaviour : MonoBehaviour, IEditorOnly
     {
         public EmoteWizardEnvironment CreateEnv()
         {
