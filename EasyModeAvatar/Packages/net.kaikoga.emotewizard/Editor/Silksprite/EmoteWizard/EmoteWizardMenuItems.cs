bb81a3ac7b6f991935b222008786be696c11d185
kaikoga <info@mail.kaikoga.net>
2023-11-26 12:24:26 +0900

    auto setup EmoteWizardRoot

--- EasyModeAvatar/Packages/net.kaikoga.emotewizard/Editor/Silksprite/EmoteWizard/EmoteWizardMenuItems.cs
+++ EasyModeAvatar/Packages/net.kaikoga.emotewizard/Editor/Silksprite/EmoteWizard/EmoteWizardMenuItems.cs
@@ -1,16 +1,22 @@
 using UnityEditor;
 using UnityEngine;
+using VRC.SDK3.Avatars.Components;
 
 namespace Silksprite.EmoteWizard
 {
     public static class EmoteWizardMenuItems
     {
         [MenuItem("GameObject/Emote Wizard", false, 20)]
-        public static void Create()
+        public static void Create(MenuCommand menuCommand)
         {
-            var gameObject = new GameObject("Emote Wizard", typeof(EmoteWizardRoot));
-            AssetDatabase.SaveAssets();
-            EditorUtility.SetDirty(gameObject);
+            var gameObject = new GameObject("Emote Wizard");
+            var root = gameObject.AddComponent<EmoteWizardRoot>();
+            if (menuCommand.context is GameObject avatar && avatar.TryGetComponent<VRCAvatarDescriptor>(out var avatarDescriptor))
+            {
+                root.transform.SetParent(avatar.transform);
+                root.avatarDescriptor = avatarDescriptor;
+            }
+            Undo.RegisterCreatedObjectUndo(gameObject, "Create Emote Wizard");
         }
     }
 }
