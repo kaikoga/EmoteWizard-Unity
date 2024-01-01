afcc8dd61c669650b3c09eca90300b6277225323
kaikoga <info@mail.kaikoga.net>
2024-01-01 14:42:23 +0900

    add editor to AnimatedBlendShapes

--- Editor/Silksprite/EmoteWizard/Sources/GenericEmoteSequenceSource.Editor.cs
+++ Editor/Silksprite/EmoteWizard/Sources/GenericEmoteSequenceSource.Editor.cs
@@ -1,8 +1,10 @@
+using System;
+using System.Linq;
 using Silksprite.EmoteWizard.DataObjects;
-using Silksprite.EmoteWizard.Sources.Impl;
 using Silksprite.EmoteWizard.Sources.Sequence;
 using Silksprite.EmoteWizard.UI;
 using Silksprite.EmoteWizard.Utils;
+using Silksprite.EmoteWizardSupport.Extensions;
 using UnityEditor;
 using UnityEngine;
 
@@ -16,6 +18,7 @@ namespace Silksprite.EmoteWizard.Sources
         SerializedProperty _serializedGroupName;
 
         SerializedProperty _serializedAnimatedEnable;
+        SerializedProperty _serializedAnimatedBlendShapes;
 
         SerializedProperty _serializedIsFixedDuration;
         SerializedProperty _serializedEntryTransitionDuration;
@@ -38,6 +41,7 @@ namespace Silksprite.EmoteWizard.Sources
             _serializedGroupName = serializedItem.FindPropertyRelative(nameof(GenericEmoteSequence.groupName));
 
             _serializedAnimatedEnable = serializedItem.FindPropertyRelative(nameof(GenericEmoteSequence.animatedEnable));
+            _serializedAnimatedBlendShapes = serializedItem.FindPropertyRelative(nameof(GenericEmoteSequence.animatedBlendShapes));
 
             _serializedIsFixedDuration = serializedItem.FindPropertyRelative(nameof(GenericEmoteSequence.isFixedDuration));
             _serializedEntryTransitionDuration = serializedItem.FindPropertyRelative(nameof(GenericEmoteSequence.entryTransitionDuration));
@@ -59,6 +63,7 @@ namespace Silksprite.EmoteWizard.Sources
             EditorGUILayout.PropertyField(_serializedGroupName);
 
             EditorGUILayout.PropertyField(_serializedAnimatedEnable);
+            EditorGUILayout.PropertyField(_serializedAnimatedBlendShapes);
 
             EditorGUILayout.PropertyField(_serializedIsFixedDuration);
             EditorGUILayout.PropertyField(_serializedEntryTransitionDuration);
@@ -96,5 +101,62 @@ namespace Silksprite.EmoteWizard.Sources
                 "",
                 "Layer Blend: VRC Playable Layer Controlの設定をします。現状Actionレイヤー専用です。",
                 "Tracking Override: VRC Animator Tracking Controlの設定をします。アニメーションの再生中に一時的にAnimationにする項目を登録します。");
+
+        [CustomPropertyDrawer(typeof(GenericEmoteSequence.AnimatedEnable))]
+        public class AnimatedEnableDrawer : PropertyDrawer
+        {
+            public override void OnGUI(Rect position, SerializedProperty serializedProperty, GUIContent label)
+            {
+                var serializedTarget = serializedProperty.FindPropertyRelative(nameof(GenericEmoteSequence.AnimatedEnable.target));
+                var serializedIsEnable = serializedProperty.FindPropertyRelative(nameof(GenericEmoteSequence.AnimatedEnable.isEnable));
+
+                EditorGUI.PropertyField(position.UISliceV(0), serializedTarget);
+                EditorGUI.PropertyField(position.UISliceV(1), serializedIsEnable);
+            }
+
+            public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+            {
+                return EditorGUIUtility.singleLineHeight * 2 + EditorGUIUtility.standardVerticalSpacing * 1;
+            }
+        }
+
+        [CustomPropertyDrawer(typeof(GenericEmoteSequence.AnimatedBlendShape))]
+        public class AnimatedBlendShapeDrawer : PropertyDrawer
+        {
+            public override void OnGUI(Rect position, SerializedProperty serializedProperty, GUIContent label)
+            {
+                var serializedTarget = serializedProperty.FindPropertyRelative(nameof(GenericEmoteSequence.AnimatedBlendShape.target));
+                var serializedBlendShapeName = serializedProperty.FindPropertyRelative(nameof(GenericEmoteSequence.AnimatedBlendShape.blendShapeName));
+                var serializedValue = serializedProperty.FindPropertyRelative(nameof(GenericEmoteSequence.AnimatedBlendShape.value));
+
+                EditorGUI.PropertyField(position.UISliceV(0), serializedTarget);
+
+                var skinnedMeshRenderer = (SkinnedMeshRenderer)serializedTarget.objectReferenceValue;
+                if (skinnedMeshRenderer && skinnedMeshRenderer.sharedMesh is Mesh sharedMesh)
+                {
+                    EditorGUI.BeginChangeCheck();
+                    var options = Enumerable.Range(0, sharedMesh.blendShapeCount)
+                        .Select(i => sharedMesh.GetBlendShapeName(i))
+                        .ToArray();
+                    var newBlendShapeNameValue = EditorGUI.Popup(
+                        position.UISliceV(1),
+                        serializedBlendShapeName.displayName,
+                        Array.IndexOf(options, serializedBlendShapeName.stringValue),
+                        options
+                    );
+                    if (EditorGUI.EndChangeCheck())
+                    {
+                        serializedBlendShapeName.stringValue = options[newBlendShapeNameValue];
+                    }
+                }
+
+                EditorGUI.PropertyField(position.UISliceV(2), serializedValue);
+            }
+
+            public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+            {
+                return EditorGUIUtility.singleLineHeight * 3 + EditorGUIUtility.standardVerticalSpacing * 2;
+            }
+        }
     }
 }
