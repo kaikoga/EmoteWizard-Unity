5fc0efa15e95d990f6db7e6578d04adc4b251283
kaikoga <info@mail.kaikoga.net>
2024-01-02 21:18:05 +0900

    complete rename before release

--- Runtime/Silksprite/EmoteWizard/Sources/Sequence/GenericEmoteSequenceSource.cs
+++ Runtime/Silksprite/EmoteWizard/Sources/Sequence/GenericEmoteSequenceSource.cs
@@ -2,14 +2,13 @@ using Silksprite.EmoteWizard.DataObjects;
 using Silksprite.EmoteWizard.Sources.Sequence.Base;
 using Silksprite.EmoteWizard.Templates.Sequence;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace Silksprite.EmoteWizard.Sources.Sequence
 {
-    [AddComponentMenu("Emote Wizard/Sources/Simple Emote Source", 101)]
+    [AddComponentMenu("Emote Wizard/Sources/Generic Emote Sequence Source", 101)]
     public class GenericEmoteSequenceSource : EmoteSequenceSourceBase
     {
-        [FormerlySerializedAs("simpleEmote")] [SerializeField] public GenericEmoteSequence sequence = new GenericEmoteSequence();
+        [SerializeField] public GenericEmoteSequence sequence = new GenericEmoteSequence();
 
         public override IEmoteSequenceFactoryTemplate ToEmoteFactoryTemplate() => new GenericEmoteSequenceFactory(sequence, $"{gameObject.name}_{gameObject.GetInstanceID()}");
     }
