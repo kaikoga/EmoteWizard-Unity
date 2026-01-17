d7764948feaabf18e306f043844382db5548222b
kaikoga <info@mail.kaikoga.net>
2026-01-17 09:53:05 +0900

    support Unity 2021

--- Editor/Silksprite/EmoteWizard/GizmoIconsDisabler.cs
+++ Editor/Silksprite/EmoteWizard/GizmoIconsDisabler.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
@@ -10,36 +9,62 @@ namespace Silksprite.EmoteWizard
 {
     public static class GizmoIconsDisabler
     {
+        static readonly Type TargetComponentBase = typeof(EmoteWizardBehaviour);
+
+        static readonly Type AnnotationUtility = Assembly.GetAssembly(typeof(Editor))
+            ?.GetType("UnityEditor.AnnotationUtility");
+
+        static readonly MethodInfo GetAnnotations = AnnotationUtility
+            ?.GetMethod("GetAnnotations", BindingFlags.Static | BindingFlags.NonPublic);
+
+        static readonly MethodInfo SetIconEnabled = AnnotationUtility
+            ?.GetMethod("SetIconEnabled", BindingFlags.Static | BindingFlags.NonPublic);
+
+        static readonly Type Annotation = Assembly.GetAssembly(typeof(Editor))
+            ?.GetType("UnityEditor.Annotation");
+
+        static readonly FieldInfo ScriptClass = Annotation
+            ?.GetField("scriptClass", BindingFlags.Instance | BindingFlags.Public);
+
         [InitializeOnLoadMethod]
         static void InitializeOnLoad()
         {
+            var a = Type.GetType("UnityEditor.AnnotationUtility");
             _ = InitializeOnLoadAsync();
         }
-
+        
         static async Task InitializeOnLoadAsync()
         {
-            await Task.Delay(TimeSpan.FromSeconds(1));
-
-            DisableGizmoIcons(AppDomain.CurrentDomain.GetAssemblies()
+            // NOTE: Unity 2021 is supported 
+            await DisableGizmoIcons(AppDomain.CurrentDomain.GetAssemblies()
                 .SelectMany(assembly => assembly.GetTypes())
-                .Where(type => typeof(EmoteWizardBehaviour).IsAssignableFrom(type) && !type.IsAbstract));
+                .Where(type => TargetComponentBase.IsAssignableFrom(type) && !type.IsAbstract)
+                .ToArray());
         }
-
+        
         const int MonoBehaviourClassId = 114; // https://docs.unity3d.com/Manual/ClassIDReference.html
-        static void DisableGizmoIcons(IEnumerable<Type> types)
+        static async Task DisableGizmoIcons(Type[] types)
         {
-            // at least I am going to support Unity 2021 later
-            var setIconEnabled = Assembly.GetAssembly(typeof(Editor))
-                ?.GetType("UnityEditor.AnnotationUtility")
-                ?.GetMethod("SetIconEnabled", BindingFlags.Static | BindingFlags.NonPublic);
-            if (setIconEnabled == null)
+            while (true)
             {
+                await Task.Delay(TimeSpan.FromSeconds(1));
+
+                if (SetIconEnabled == null)
+                {
+                    return;
+                }
+                    
+                var annotations = ((Array)GetAnnotations.Invoke(null, new object[] { })).Cast<object>();
+                if (annotations.All(annotation => (string)ScriptClass.GetValue(annotation) != types[0].Name))
+                {
+                    continue;
+                }
+
+                foreach (var type in types)
+                {
+                    SetIconEnabled.Invoke(null, new object[] {MonoBehaviourClassId, type.Name, 0});
+                }
                 return;
             }
-            foreach (var type in types)
-            {
-                setIconEnabled.Invoke(null, new object[] {MonoBehaviourClassId, type.Name, 0});
-            }
-        }
-    }
+        }    }
 }
