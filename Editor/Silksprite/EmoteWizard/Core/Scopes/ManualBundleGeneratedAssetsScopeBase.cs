86d0865c10df5ba3c83fc5780e0518fa4c291986
kaikoga <info@mail.kaikoga.net>
2026-03-11 08:49:00 +0900

    be hybrid

--- Editor/Silksprite/EmoteWizard/Core/Scopes/ManualBundleGeneratedAssetsScopeBase.cs
+++ Editor/Silksprite/EmoteWizard/Core/Scopes/ManualBundleGeneratedAssetsScopeBase.cs
@@ -1,13 +1,20 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using Ablet.Building.Ephemeral;
 using Silksprite.EmoteWizard.Contexts;
 using Object = UnityEngine.Object;
 
-#if EW_NDMF_SUPPORT
+#if EW_NDMF_SUPPORT || EW_ABLET_SUPPORT
 using UnityEditor;
 using UnityEngine;
+#endif
+
+#if EW_NDMF_SUPPORT
 using nadena.dev.ndmf;
 using nadena.dev.ndmf.util;
+#elif EW_ABLET_SUPPORT
+using Ablet.Building;
 #endif
 
 namespace Silksprite.EmoteWizard.Scopes
@@ -27,6 +34,7 @@ namespace Silksprite.EmoteWizard.Scopes
 #if EW_NDMF_SUPPORT
                 _gameObject = new GameObject("Temporary");
                 _buildContext = new BuildContext(_gameObject, "Assets/ZZZ_GeneratedAssets/__EmoteWizard");
+#elif EW_ABLET_SUPPORT
 #else
                 throw new InvalidOperationException("");
 #endif
@@ -50,6 +58,14 @@ namespace Silksprite.EmoteWizard.Scopes
 
             AssetDatabase.SaveAssets();
             Object.DestroyImmediate(_gameObject);
+#elif EW_ABLET_SUPPORT
+            var generatedAssets = CollectVolatileAssets(_environment)
+                .SelectMany(AssetPersister.IterateAssetObjectReferences)
+                .Distinct()
+                .Where(obj => !EditorUtility.IsPersistent(obj));
+            
+            var assetPersister = new AssetPersisterState(_environment.AvatarRoot.gameObject, true);
+            assetPersister.PersistAssets(generatedAssets);
 #endif
         }
 
