19fbd59e8c22d7c2915e785150526c952285ffaf
kaikoga <info@mail.kaikoga.net>
2026-03-19 04:57:00 +0900

    return EditorFeatures of ChilloutVR to platform asmdef

--- Editor/Silksprite/EmoteWizard/Platforms/Common/NullEditorPlatformFeatures.cs
+++ Editor/Silksprite/EmoteWizard/Platforms/Common/NullEditorPlatformFeatures.cs
@@ -1,11 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using Silksprite.AdLib.ChilloutVR;
-using Silksprite.AdLib.ChilloutVR.Access;
 using Silksprite.EmoteWizard.DataObjects;
-using Silksprite.EmoteWizard.Platforms.Common.Extensions;
-using Silksprite.EmoteWizard.Platforms.Common.Internal.Extensions;
 using UnityEditor.Animations;
 
 namespace Silksprite.EmoteWizard.Platforms.Common
