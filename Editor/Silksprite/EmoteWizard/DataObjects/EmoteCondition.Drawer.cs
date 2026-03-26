d6645c3ea70f29206fcfadf9d03951d3623c7fdb
kaikoga <info@mail.kaikoga.net>
2026-03-26 09:53:33 +0900

    enhance drawers a little

--- Editor/Silksprite/EmoteWizard/DataObjects/EmoteCondition.Drawer.cs
+++ Editor/Silksprite/EmoteWizard/DataObjects/EmoteCondition.Drawer.cs
@@ -1,5 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Silksprite.EmoteWizardSupport.Extensions;
 using Silksprite.EmoteWizardSupport.Scopes;
+using Silksprite.EmoteWizardSupport.Tools;
+using Silksprite.Loch;
 using Silksprite.Loch.Extensions;
 using Silksprite.Loch.IMGUI;
 using UnityEditor;
@@ -11,6 +16,49 @@ namespace Silksprite.EmoteWizard.DataObjects
     [CustomPropertyDrawer(typeof(EmoteCondition))]
     public class EmoteConditionDrawer : PropertyDrawer
     {
+        static readonly Dictionary<ParameterItemKind, EmoteConditionMode[]> Modes = new Dictionary<ParameterItemKind, EmoteConditionMode[]>
+        {
+            [ParameterItemKind.Auto] = new []
+            {
+                EmoteConditionMode.If,
+                EmoteConditionMode.IfNot,
+                EmoteConditionMode.Greater,
+                EmoteConditionMode.Less,
+                EmoteConditionMode.Equals,
+                EmoteConditionMode.NotEqual,
+            },
+            [ParameterItemKind.Bool] = new []
+            {
+                EmoteConditionMode.If,
+                EmoteConditionMode.IfNot,
+            },
+            [ParameterItemKind.Int] = new []
+            {
+                EmoteConditionMode.Greater,
+                EmoteConditionMode.Less,
+                EmoteConditionMode.Equals,
+                EmoteConditionMode.NotEqual,
+            },
+            [ParameterItemKind.Float] = new []
+            {
+                EmoteConditionMode.Greater,
+                EmoteConditionMode.Less,
+            },
+        };
+
+        static readonly Dictionary<EmoteConditionMode[], string[]> DisplayModes = new Dictionary<EmoteConditionMode[], string[]>();
+
+        static string[] GetDisplayModes(EmoteConditionMode[] modes)
+        {
+            if (!DisplayModes.TryGetValue(modes, out var displayModes))
+            {
+                // FIXME: Loch
+                displayModes = modes.Select(mode => $"{mode}").ToArray();
+                DisplayModes.Add(modes, displayModes);
+            }
+            return displayModes;
+        }
+
         public override void OnGUI(Rect position, SerializedProperty serializedProperty, GUIContent label)
         {
             var parameter = serializedProperty.Lop(nameof(EmoteCondition.parameter), Loc("EmoteCondition::parameter"));
@@ -18,14 +66,46 @@ namespace Silksprite.EmoteWizard.DataObjects
             var mode = serializedProperty.Lop(nameof(EmoteCondition.mode), Loc("EmoteCondition::mode"));
             var threshold = serializedProperty.Lop(nameof(EmoteCondition.threshold), Loc("EmoteCondition::threshold"));
 
-            using (new EditorGUI.IndentLevelScope())
+            var top = position.UISliceV(0);
+            var bottom = position.UISliceV(1);
             using (new HideLabelsScope())
             {
-                LEditorGUI.Prop(position.UISliceH(0.0f, 0.4f), parameter);
-                LEditorGUI.Prop(position.UISliceH(0.4f, 0.2f), kind);
-                LEditorGUI.Prop(position.UISliceH(0.6f, 0.2f), mode);
-                LEditorGUI.Prop(position.UISliceH(0.8f, 0.2f), threshold);
+                LEditorGUI.Prop(top.UISliceH(0.0f, 0.6f), parameter);
+                LEditorGUI.Prop(top.UISliceH(0.6f, 0.4f), kind);
+                ModePopup(bottom.UISliceH(0.1f, 0.4f), mode, (ParameterItemKind)kind.Property.intValue);
+                LEditorGUI.Prop(bottom.UISliceH(0.5f, 0.5f), threshold);
             }
+        }
+
+        static void ModePopup(Rect position, LocalizedProperty mode, ParameterItemKind kind)
+        {
+            EditorGUI.BeginProperty(position, mode.Loc.GUIContent, mode.Property);
+            if (!Modes.TryGetValue(kind, out var modes))
+            {
+                modes = Array.Empty<EmoteConditionMode>();
+            }
+            var modeValue = (EmoteConditionMode)mode.Property.intValue;
+            var modeIndex = Array.IndexOf(modes, modeValue);
+            var isInvalid = false;
+            if (modeIndex == -1)
+            {
+                ArrayUtility.Add(ref modes, modeValue);
+                modeIndex = modes.Length - 1;
+                isInvalid = true;
+            }
+            using var _ = new InvalidValueScope(isInvalid);
+            EditorGUI.BeginChangeCheck();
+            modeIndex = EditorGUI.Popup(position, modeIndex, GetDisplayModes(modes).ToArray());
+            if (EditorGUI.EndChangeCheck())
+            {
+                mode.Property.intValue = (int)modes[modeIndex];
+            }
+            EditorGUI.EndProperty();
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return PropertyDrawerUITools.LineHeight(2);
         }
     }
 }
--- Editor/Silksprite/EmoteWizard/DataObjects/ParameterNameAttribute.Drawer.cs
+++ Editor/Silksprite/EmoteWizard/DataObjects/ParameterNameAttribute.Drawer.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Silksprite.EmoteWizard.Contexts;
 using Silksprite.EmoteWizard.Scopes;
 using Silksprite.EmoteWizard.Utils;
@@ -18,18 +19,39 @@ namespace Silksprite.EmoteWizard.DataObjects
 
         static void PropertyField(Rect position, SerializedProperty serializedProperty, GUIContent label, bool allowEmpty, bool allowNew)
         {
+            var left = position;
+            left.width -= EditorGUIUtility.singleLineHeight;
+            var right = position;
+            right.xMin = right.xMax - EditorGUIUtility.singleLineHeight;
+
+            var indent = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+            EditorGUI.BeginProperty(position, label, serializedProperty);
+
             var isInvalidValue = ParameterNameUtil.IsInvalidParameterFormat(serializedProperty.stringValue, allowEmpty);
-            
+
+            var snapshot = InnerGUIEnvironmentScope.GetCurrentEnv().GetContext<ParametersContext>().Snapshot();
             if (!allowNew)
             {
-                var env = InnerGUIEnvironmentScope.GetCurrentEnv();
-                isInvalidValue |= env.GetContext<ParametersContext>().Snapshot().IsInvalidParameterReference(serializedProperty.stringValue);
+                isInvalidValue |= snapshot.IsInvalidParameterReference(serializedProperty.stringValue);
             }
 
             using (new InvalidValueScope(isInvalidValue))
             {
-                EditorGUI.PropertyField(position, serializedProperty, label);
+                serializedProperty.stringValue = EditorGUI.TextField(left, label, serializedProperty.stringValue);
+                EditorGUI.BeginChangeCheck();
+                var filteredParameters = snapshot.AllParameters.Where(parameter => parameter.name.StartsWith(serializedProperty.stringValue)).ToArray();
+                var parameterNames = filteredParameters.Select(parameter => parameter.name).ToArray();
+                var parameterDisplayNames = filteredParameters.Select(parameter => $"{parameter.name} ({parameter.itemKind})").ToArray();
+                var index = EditorGUI.Popup(right, -1, parameterDisplayNames);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    serializedProperty.stringValue = parameterNames[index];
+                    GUI.changed = true; // propagate
+                }
             }
+            EditorGUI.EndProperty();
+            EditorGUI.indentLevel = indent;
         }
     }
 }
