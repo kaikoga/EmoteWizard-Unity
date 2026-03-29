b6718f7e1ae9d6bfb7acf861eab01ced4d5806e3
kaikoga <info@mail.kaikoga.net>
2026-03-29 15:26:11 +0900

    log to Ablet

--- Editor/Silksprite/EmoteWizardSupport/Logger/ErrorReportWrapper.cs
+++ Editor/Silksprite/EmoteWizardSupport/Logger/ErrorReportWrapper.cs
@@ -1,9 +1,14 @@
+using Ablet.ErrorReporting;
 using Silksprite.Loch;
 using UnityEngine;
 
 #if EW_NDMF_SUPPORT
+using Ablet.API;
 using nadena.dev.ndmf;
+using NdmfErrorReport = nadena.dev.ndmf.ErrorReport;
 #endif
+
+using AbletErrorReport = Ablet.ErrorReporting.ErrorReport;
 
 namespace Silksprite.EmoteWizardSupport.Logger
 {
@@ -12,19 +17,26 @@ namespace Silksprite.EmoteWizardSupport.Logger
         public static void LogWarningFormat(LocalizedContent loc, Substitution substitution)
         {
 #if EW_NDMF_SUPPORT
-            ErrorReport.ReportError(new WrappedError(ErrorSeverity.NonFatal, loc, null, substitution));
-#else
-            Debug.LogWarningFormat(substitution.Format(loc.Tr), substitution);
+            if (!AbletSymbols.PreferAblet)
+            {
+                NdmfErrorReport.ReportError(new WrappedError(ErrorSeverity.NonFatal, loc, null, substitution));
+                return;
+            }
 #endif
+            AbletErrorReport.LogWarning(substitution.Format(loc.Tr));
         }
 
         public static void LogWarningFormat(LocalizedContent loc, Object target, Substitution substitution)
         {
 #if EW_NDMF_SUPPORT
-            ErrorReport.ReportError(new WrappedError(ErrorSeverity.NonFatal, loc, target, substitution));
-#else
-            Debug.LogWarningFormat(substitution.Format(loc.Tr), target);
+            if (!AbletSymbols.PreferAblet)
+            {
+                NdmfErrorReport.ReportError(new WrappedError(ErrorSeverity.NonFatal, loc, target, substitution));
+                return;
+            }
 #endif
+            using var _ = new InterestScope(target);
+            AbletErrorReport.LogWarning(substitution.Format(loc.Tr));
         }
     }
 }
