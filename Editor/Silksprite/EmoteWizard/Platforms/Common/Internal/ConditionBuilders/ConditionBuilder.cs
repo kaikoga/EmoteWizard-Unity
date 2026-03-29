678e5f1069e84d263811c059cdf0b0c43b5cd843
kaikoga <info@mail.kaikoga.net>
2026-03-29 10:01:50 +0900

    delete unused shortcuts

--- Editor/Silksprite/EmoteWizard/Platforms/Common/Internal/ConditionBuilders/ConditionBuilder.cs
+++ Editor/Silksprite/EmoteWizard/Platforms/Common/Internal/ConditionBuilders/ConditionBuilder.cs
@@ -58,16 +58,6 @@ namespace Silksprite.EmoteWizard.Platforms.Common.Internal.ConditionBuilders
             }
         }
 
-        public static ConditionBuilder IfCondition(string paramName, bool value) => new ConditionBuilder().If(paramName, value);
-        public static ConditionBuilder NotEqualCondition(string paramName, int value) => new ConditionBuilder().NotEqual(paramName, value);
-        public static ConditionBuilder EqualsCondition(string paramName, int value) => new ConditionBuilder().Equals(paramName, value);
-        public static ConditionBuilder LessCondition(string paramName, float value) => new ConditionBuilder().Less(paramName, value);
-        public static ConditionBuilder ApproximateCondition(string paramName, float value) => new ConditionBuilder().Approximate(paramName, value);
-        public static ConditionBuilder InclusiveRangeCondition(string paramName, int? min, int? max) => new ConditionBuilder().InclusiveRange(paramName, min, max);
-        public static ConditionBuilder InclusiveRangeCondition(string paramName, float? min, float? max) => new ConditionBuilder().InclusiveRange(paramName, min, max);
-        public static ConditionBuilder ExclusiveRangeCondition(string paramName, int? min, int? max) => new ConditionBuilder().ExclusiveRange(paramName, min, max);
-        public static ConditionBuilder ExclusiveRangeCondition(string paramName, float? min, float? max) => new ConditionBuilder().ExclusiveRange(paramName, min, max);
-
         struct ConditionElement
         {
             public AnimatorControllerParameterType ParameterType; 
